948ad1121c616e20d2e22feb2ce93ab095503eef
Haoze Chen <albertchen2017@outlook.com>
2023-07-23 14:16:53 -0700

    add pages

---
+++ nuxt-dojo/app.vue
@@ -0,0 +1,31 @@
+<template>
+  <div>
+    <h2>App</h2>
+    <p>
+      Lorem, ipsum dolor sit amet consectetur adipisicing elit. Commodi possimus
+      eveniet deleniti mollitia fuga architecto sapiente quidem, molestiae quasi
+      velit voluptatum laborum ducimus accusamus laboriosam cupiditate nulla
+      aspernatur vitae. Provident.
+    </p>
+    <p>
+      Lorem, ipsum dolor sit amet consectetur adipisicing elit. Commodi possimus
+      eveniet deleniti mollitia fuga architecto sapiente quidem, molestiae quasi
+      velit voluptatum laborum ducimus accusamus laboriosam cupiditate nulla
+      aspernatur vitae. Provident.
+    </p>
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style scoped>
+h2 {
+  margin-bottom: 20px;
+  font-size: 36px;
+}
+p {
+  margin: 20px 0;
+}
+</style>
---
+++ nuxt-dojo/pages/about.vue
@@ -0,0 +1,33 @@
+<template>
+  <div>
+    <div>
+      <h2>About</h2>
+      <p>
+        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Commodi
+        possimus eveniet deleniti mollitia fuga architecto sapiente quidem,
+        molestiae quasi velit voluptatum laborum ducimus accusamus laboriosam
+        cupiditate nulla aspernatur vitae. Provident.
+      </p>
+      <p>
+        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Commodi
+        possimus eveniet deleniti mollitia fuga architecto sapiente quidem,
+        molestiae quasi velit voluptatum laborum ducimus accusamus laboriosam
+        cupiditate nulla aspernatur vitae. Provident.
+      </p>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style scoped>
+h2 {
+  margin-bottom: 20px;
+  font-size: 36px;
+}
+p {
+  margin: 20px 0;
+}
+</style>
